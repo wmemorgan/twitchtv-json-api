d2baf6e6380a9fd16bd2b8cecc765e6b3dd1962e
Wil Morgan <wil.me.morgan@gmail.com>
2017-12-24 07:19:54 +0800

    Add boolean function for determining if someone is online or offline.

--- script.js
+++ script.js
@@ -19,6 +19,23 @@ function apiData(endpoint) {
     }); // Ends getJSON API channels data call 
 }
 
+function isOnline(data, game, status) {
+    if (data["stream"] == null) {
+        $('#twitchtv_results').append('<div class="col-lg-3 col-sm-6 boxy current_status" id="game"'
+            + '<h5>offline</h5>'
+            + '</div>'
+            + '</div>');
+    } else {
+        $('#twitchtv_results').append('<div class="col-lg-3 col-sm-6 boxy current_status" id="game">'
+            + game
+            + '</div>'
+            + '<div class="col-lg-3 col-sm-6 boxy current_status" id="status">'
+            + status
+            + '</div>'
+            + '</div>');
+    }
+}
+
 function displayResults(logo) {
     $('#twitchtv_results').append('<div class="col-lg-3 col-sm-6 boxy current_status" id="display_name">'
         + '<a href="' + channel_url + '" target="_blank">' + display_name + '</a>' + '</div>');
@@ -27,10 +44,10 @@ function displayResults(logo) {
 
 $(document).ready(function() {
     var usernames = ["freecodecamp", "esl_sc2", "dotastarladder_en","guit88man"]
-    var category = "channels"
+    // var category = "channels"
 
     for (i in usernames) {
-        var url = apiEndpoint(usernames[i],category);
+        var url = apiEndpoint(usernames[i], "channels");
         console.log(url);
 
         // Execute Twitch.tv API call and download channels data        
@@ -38,76 +55,31 @@ $(document).ready(function() {
             console.log(data);
             // Parse query results
             var logo = data["logo"];
-            console.log(logo);
+            // console.log(logo);
             var display_name = data["display_name"];
-            console.log(display_name);
+            // console.log(display_name);
             var game = data["game"];
-            console.log(game);
+            // console.log(game);
             var status = data["status"];
-            console.log(status)
+            // console.log(status)
             var channel_url = data["url"];
-            console.log(channel_url);
+            // console.log(channel_url);
+            
+            // $('#twitchtv_results').append('<div class="row">'
+            //     + '<div class="col-lg-3 col-sm-6 boxy current_status" id="logo">'
+            //     + '<img src="' + logo + '" height="50" width="50">' + '</div>'
+            //     + '<div class="col-lg-3 col-sm-6 boxy current_status" id="display_name">'
+            //     + '<a href="' + channel_url + '" target="_blank">' + display_name + '</a>'
+            //     + '</div>');
 
             // Identify whether user live streaming
-            $.getJSON(apiEndpoint(usernames[i], "streams"), function (stream_data) {
+            var url2 = apiEndpoint(usernames[i], "streams");
+            $.getJSON(url2, function (stream_data) {
                 //Validate api data
-                console.log(apiEndpoint(usernames[i], "streams"));
                 console.log(stream_data);
 
-            if (stream_data["stream"] == null) {
-        
-        // Inject query results and hyperlink to channel page
-        // $('#twitchtv_results').html('<h1>Twitch.tv</h1>')
-        // $('#twitchtv_results').append('<div class="col-lg-3 col-sm-6 boxy current_status" id="logo">'
-        //     + '<img src="' + logo +'" height="50" width="50">' + '</div>');
-                $('#twitchtv_results').append('<div class="row">'
-                + '<div class="col-lg-3 col-sm-6 boxy current_status" id="logo">'
-                + '<img src="' + logo + '" height="50" width="50">' + '</div>'
-                + '<div class="col-lg-3 col-sm-6 boxy current_status" id="display_name">'
-                + '<a href="' + channel_url + '" target="_blank">' + display_name + '</a>' 
-                + '</div>'
-                + '<div class="col-lg-3 col-sm-6 boxy current_status" id="game"'
-                + '<h5>offline</h5>'
-                + '</div>' 
-                + '</div>');
-        } else {
-                $('#twitchtv_results').append('<div class="row">'
-                    + '<div class="col-lg-3 col-sm-6 boxy current_status" id="logo">'
-                    + '<img src="' + logo + '" height="50" width="50">' + '</div>'
-                    + '<div class="col-lg-3 col-sm-6 boxy current_status" id="display_name">'
-                    + '<a href="' + channel_url + '" target="_blank">' + display_name + '</a>'
-                    + '</div>'
-                    + '<div class="col-lg-3 col-sm-6 boxy current_status" id="game">'
-                    + game
-                    + '</div>'
-                    + '<div class="col-lg-3 col-sm-6 boxy current_status" id="status">'
-                    + status
-                    + '</div>'
-                    + '</div>');
-        }
+                isOnline(stream_data, game, status);
 
-        // displayResults(logo);
-
-
-
-            // if (stream_data["stream"] == null) {
-            //     // $('.current_status').css({"background-color":"red"}); 
-            //     $('#game').append(
-            //         +'<div class="col-lg-3 col-sm-6 boxy current_status" id="game"'
-            //         +'<h5>offline</h5>'
-            //         +'</div>'
-            //         );
-            //     $('#status').hide();
-            // } else {
-            //     // $('.current_status').css("background-color", "green"); 
-            //     $('#game').append('<div class="row">'
-            //     +'<div class="col-lg-3 col-sm-6 boxy current_status" id="game">'
-            //     +game
-            //     +'</div>'
-            //     +'</div>');
-            //     $('#status').append(": " + status);
-
-            // } // Ends if else statement
         }); // Ends getJSON API streams data call
      }); // Ends getJSON API channels data call 
     } // End of for loop
