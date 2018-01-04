d4c3d2eca220ab13658104508e135058287ea108
Wil Morgan <wil.me.morgan@gmail.com>
2018-01-04 23:31:18 +0800

    Remove extraneous verification 'console.log' statements.

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 
 function apiEndpoint(username, category) {
-    // console.log(username);
-    // console.log(category);
     // Declare API endpoints
     var endpoint = "https://wind-bow.gomix.me/twitch-api/"
         + category + "/" + username + "?callback=?";
-    // console.log(endpoint);
     return endpoint;
 }
 
 function apiData(endpoint) {
+    // Call API data
     var jsonObject = $.getJSON(endpoint, function (data) {
         console.log(data);
     });
@@ -18,15 +16,11 @@ function apiData(endpoint) {
 
 function displayStreamInfo(data) {
     var logo = data["stream"]["channel"]["logo"];
-    // console.log(logo);
     var display_name = data["stream"]["channel"]["display_name"];
-    // console.log(display_name);
     var game = data["stream"]["channel"]["game"];
-    // console.log(game);
     var preview = data["stream"]["channel"]["status"];
-    // console.log(status)
     var channel_url = data["stream"]["channel"]["url"];
-    // console.log(channel_url);
+    // Display stream data
     $('.grid').append('<div class="logo">' + '<img src="' + logo + '" height="50" width="50">' + '</div>'
         + '<div class="display_name">'
         + '<a href="' + channel_url + '" target="_blank">'
@@ -36,15 +30,11 @@ function displayStreamInfo(data) {
 
 function displayChannelInfo(data) {
     var logo = data["logo"];
-    // console.log(logo);
     var display_name = data["display_name"];
-    // console.log(display_name);
     var game = data["game"];
-    // console.log(game);
     var preview = data["status"];
-    // console.log(status)
     var channel_url = data["url"];
-    // console.log(channel_url);
+    // Display channel data
     $('.grid').append('<div class="logo">' + '<img src="' + logo + '" height="50" width="50">' + '</div>'
         + '<div class="display_name">'
         + '<a href="' + channel_url + '" target="_blank">'
@@ -53,7 +43,6 @@ function displayChannelInfo(data) {
 }
 
 var usernames = ["freecodecamp", "esl_sc2", "dotastarladder_en", "guit88man", "jasonr"]
-// var i = 4;
 
 for (i in usernames) {
 
@@ -63,19 +52,20 @@ var channelEndpoint = apiEndpoint(usernames[i], "channels")
 $.when(
     apiData(streamEndpoint),
     apiData(channelEndpoint)
-).done(function (streamData, channelData) { // handle the data they return here
-    // do stuff with your data
-    var streamJSON = streamData[2]["responseJSON"];
-    var channelJSON = channelData[2]["responseJSON"];
+).done(function (streamJSON, channelJSON) {
+   
+    var streamData = streamJSON[2]["responseJSON"];
+    var channelData = channelJSON[2]["responseJSON"];
 
-    if (streamData[0]["stream"] == null) {
-        console.log(channelJSON["display_name"], "is offline")
-        console.log(channelJSON);
-        displayChannelInfo(channelJSON)
+    // Evaluate if user is streaming live
+    if (streamData["stream"] == null) {
+        console.log(channelData["display_name"], "is offline")
+        console.log(channelData);
+        displayChannelInfo(channelData)
     } else {
-        console.log(streamJSON["stream"]["channel"]["display_name"], "is online")
-        console.log(streamJSON);
-        displayStreamInfo(streamJSON);
+        console.log(streamData["stream"]["channel"]["display_name"], "is online")
+        console.log(streamData);
+        displayStreamInfo(streamData);
     }
 
 });
